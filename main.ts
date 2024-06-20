29da9f8eb13a301c9d10c4da21e7f32a03cfa3a3
Rory Thomson <rthomson700@gmail.com>
2024-06-20 20:57:43 +1200

    Basic prototype built

---
+++ main.ts
@@ -0,0 +1,72 @@
+import { readFileSync, writeFileSync, openSync } from "fs";
+const path = "./trackpoll.rvt.json";
+const json = JSON.parse(readFileSync(path, "utf8"));
+const flights = json.flights;
+
+interface trackPoint {
+  timestamp: number;
+  coord: [number, number];
+  gs: number;
+  alt: number;
+  type: string;
+  isolated: boolean;
+}
+interface point {
+  type: string;
+
+  time: number;
+}
+interface altPoint extends point {
+  type: "alt";
+  alt: number;
+}
+interface gsPoint extends point {
+  type: "gs";
+  gs: number;
+}
+interface coord extends point {
+  type: "coord";
+  coord: [number, number];
+}
+interface points {
+  altPoints: altPoint[];
+  gsPoints: gsPoint[];
+  coords: coord[];
+}
+Object.values(flights).forEach((flight: any, flightIndex: number) => {
+  const track = flight.track as trackPoint[];
+  const processed_track = track.reduce(
+    (acc: points, curr) => {
+      const time = curr.timestamp;
+      acc.altPoints.push({ alt: curr.alt, time: time, type: "alt" });
+      acc.gsPoints.push({ gs: curr.gs, time: time, type: "gs" });
+      acc.coords.push({ coord: curr.coord, time: time, type: "coord" });
+      return acc;
+    },
+    { altPoints: [], gsPoints: [], coords: [] } as points
+  );
+
+  type pointTypes = gsPoint[] | altPoint[] | coord[];
+  Object.entries(processed_track).forEach((entry: [string, pointTypes]) => {
+    const fileName = "./" + entry[0] + "_" + flightIndex + ".csv";
+    const file = openSync(fileName, "w");
+    entry[1].forEach((point) => {
+      let str = new Date(point.time) + ",";
+      switch (point.type) {
+        case "alt":
+          str += point.alt + "\n";
+          break;
+        case "gs":
+          str += point.gs + "\n";
+          break;
+        case "coord":
+          str += point.coord[0] + "," + point.coord[1] + "\n";
+      }
+      writeFileSync(file, str);
+    });
+  });
+
+  //   console.log(processed_track);
+});
+// console.log(json.flights.track);
+// console.log(path);
